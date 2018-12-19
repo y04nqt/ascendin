07c67fa3d1def3ec07e0a18f2dc7452a4d4f4d2b
Aaron <aaron@doublemap.com>
2018-12-18 23:56:44 -0500

    fixed some wording and styles

--- static/js/app/Components/Projects.jsx
+++ static/js/app/Components/Projects.jsx
@@ -10,7 +10,7 @@ export default class Projects extends React.Component {
         return(
             <div id="projects" className="page">
                 <ol>
-                    <li id="build-profile-container" className="featureDescriptionContainers">
+                    <li id="work-experience-container" className="featureDescriptionContainers">
                         <section className="featureDescriptionContentWrapper">
                             <h2>Work Experience</h2>
                             <p>Frontend Web Engineer - <a id="spot-link" class="work-links" href="https://spotinc.com/" target="_blank">Spot Inc</a></p>
@@ -48,10 +48,10 @@ export default class Projects extends React.Component {
                             <p>Gym Management & Automation</p>
                         </section>
                     </li>
-                    <li id="friend-finder-container" className="featureDescriptionContainers">
+                    <li id="hello-universe-project" className="featureDescriptionContainers">
                         <section className="featureDescriptionContentWrapper">
                             <h2>Hello, Universe!</h2>
-                            <p><a>Channel link</a></p>
+                            <p><a id="channel-link" href="https://www.youtube.com/channel/UC_Cazf0InKheWZYtSEtz0Og" target="_blank">Check out the channel here!</a></p>
                             <p>This channel centers around my climbing hobby and programming education. Here I upload code tutorials, podcasts, and things that interest me in general. Some videos may be food-centric, climbing feats, or code topics.</p>
                             <p>I hope to make this channel into a place where I can express my career interests and hobbies. As I grow, I expect it to become a marketing tool for merchandise focused around programming and climbing with deep artistic designs. I believe this will be a platform for me to create informative and inspirational videos to propel my teaching abilities preparing me for my ultimate dream of creating a positive impact on the world.</p>
                         </section>
